4f19c29aded9ec9d34e1d9ff97034809f546475a
SiriuSStar <siriusstar@mains-Laptop.local>
2024-10-12 00:35:24 -0400

    Revised CG.m EN-version
    Two markdown problems still remained, one is how to use **Markdown** in the **title**, another one is that ## how to insert _inline broke marks_ to stop being decorated with heading.

--- Chapter_six_Subject_two/CG.m
+++ Chapter_six_Subject_two/CG.m
@@ -1,26 +1,56 @@
-function [x,steps] = CG(A,b,x0,eps)
-r0 = b - A*x0;
-p0 = r0;
+function [x, steps] = CG(A, b, x0, eps)
+% CG - Conjugate Gradient method for solving Ax = b
+%
+% Inputs:
+% A    - Coefficient matrix
+% b    - Right-hand side vector
+% x0   - Initial guess for the solution
+% eps  - Tolerance for the stopping criterion (optional)
+%
+% Outputs:
+% x     - Solution vector
+% steps - Number of iterations taken to converge
+
+r0 = b - A*x0;  % Initial residual
+p0 = r0;        % Initial direction is set to the residual
+
+% If only 3 inputs are provided, set the default tolerance
 if nargin == 3
     eps = 1.0e-6;
 end
+
 steps = 0;
+
 while 1
+    % Check the stopping criterion based on the norm of the current direction
     if abs(p0) < eps
         break;
     end
+    
     steps = steps + 1;
-    a0 = r0'*r0/(p0'*A*p0);%多次用到可以存一步。
-    x1 = x0 + a0*p0;
-    r1 = r0 -a0*A*p0;
-    b0 = r1'*r1/(r0'*r0);
-    %这里的r'* r虽然后面可能还会用到，但是由于计算量不大，没有必要再设个新变量将
-    %其存下了，内存上的开销划不来。
-    p1 = r1 + b0*p0;
-    %只是用到前后两层的向量，所以可以往回覆盖
+    
+    % Compute step size alpha (r0'*r0 can be reused later, store it)
+    a0 = r0' * r0 / (p0' * A * p0);
+    
+    x1 = x0 + a0 * p0
+    
+    r1 = r0 - a0 * A * p0;
+    
+    % Compute beta for the next direction
+    b0 = r1' * r1 / (r0' * r0);
+    
+    % Although r'*r might be reused later, it is not computationally expensive, 
+    % so there's no need to store it as a new variable.
+    
+    % Update direction
+    p1 = r1 + b0 * p0;
+    
+    % Since only the previous and current vectors are needed, we can overwrite them
     x0 = x1;
     r0 = r1;
     p0 = p1;
 end
+
+% Return the final solution
 x = x0;
 end
